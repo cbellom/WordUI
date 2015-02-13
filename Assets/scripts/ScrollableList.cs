f241033836e3ada81fe71f6b8ad3e661e8036e36
cbellom <cbellom@unal.edu.co>
2015-02-13 12:33:05 -0500

    generacion de itemList dinamicamente

--- Assets/scripts/ScrollableList.cs
+++ Assets/scripts/ScrollableList.cs
@@ -13,22 +13,6 @@ public class ScrollableList : MonoBehaviour
     void Start(){
 		itemCount = levelData.words.Count;
 
-        RectTransform rowRectTransform = itemPrefab.GetComponent<RectTransform>();
-        RectTransform containerRectTransform = gameObject.GetComponent<RectTransform>();
-
-        //calculate the width and height of each child item.
-        float width = containerRectTransform.rect.width / columnCount;
-        float ratio = width / rowRectTransform.rect.width;
-        float height = rowRectTransform.rect.height * ratio;
-        int rowCount = itemCount / columnCount;
-        if (itemCount % rowCount > 0)
-            rowCount++;
-
-        //adjust the height of the container so that it will just barely fit all its children
-        float scrollHeight = height * rowCount;
-        containerRectTransform.offsetMin = new Vector2(containerRectTransform.offsetMin.x, -scrollHeight / 2);
-        containerRectTransform.offsetMax = new Vector2(containerRectTransform.offsetMax.x, scrollHeight / 2);
-
         int j = 0;
 		for (int i = 0; i < itemCount; i++){
 			//this is used instead of a double for loop because itemCount may not fit perfectly into the rows/columns
@@ -37,23 +21,11 @@ public class ScrollableList : MonoBehaviour
 			
 			//create a new item, name it, and set the parent
 			GameObject newItem = Instantiate(itemPrefab) as GameObject;
-			//newItem.name = gameObject.name + " item at (" + i + "," + j + ")";
 			newItem.name = "itemWord_" + i;
 			newItem.transform.SetParent(gameObject.transform);
 			
 			newItem.transform.GetChild(0).GetComponent<Text>().text = levelData.words[i].wordText;
-			newItem.transform.GetChild(1).GetComponent<Image>().sprite = levelData.words[i].wordImage;			
-			
-			//move and size the new item
-			RectTransform rectTransform = newItem.GetComponent<RectTransform>();
-			
-			float x = -containerRectTransform.rect.width / 2 + width * (i % columnCount);
-			float y = containerRectTransform.rect.height / 2 - height * j;
-			rectTransform.offsetMin = new Vector2(x, y);
-			
-			x = rectTransform.offsetMin.x + width;
-			y = rectTransform.offsetMin.y + height;
-			rectTransform.offsetMax = new Vector2(x, y);		
+			newItem.transform.GetChild(1).GetComponent<Image>().sprite = levelData.words[i].wordImage;		
 
         }
 		Destroy (itemPrefab);
